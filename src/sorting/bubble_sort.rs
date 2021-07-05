70cdda2b37f1a281e3b8fc92758ae858b4e246f6
Alexander Gonzalez <alexfertel97@gmail.com>
2021-07-05 16:32:22 -0400

    test: Improve bubble_sort tests

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -13,8 +13,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sorts() {
-        let mut array = [5, 4, 123, 5, 5462, 0];
+    fn basic() {
+        let mut array = [5, 4, 1, 6, 0];
+        bubble_sort(&mut array);
+        assert_sorted!(&array);
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut array = [5, 5, 1, 6, 1, 0, 2, 6];
+        bubble_sort(&mut array);
+        assert_sorted!(&array);
+    }
+
+    #[test]
+    fn pre_sorted() {
+        let mut array = [1, 2, 3, 4, 5, 6];
         bubble_sort(&mut array);
         assert_sorted!(&array);
     }

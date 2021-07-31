d43d2f1f5f25b5d208105680d5c2f3fbc2b451d7
agris <33702214+agrism@users.noreply.github.com>
2021-07-31 17:51:11 +0300

    Extend Counting Sort to support any object keyed by integer (#17)

--- src/sorting/counting_sort.rs
+++ src/sorting/counting_sort.rs
@@ -1,11 +1,11 @@
-pub fn counting_sort(array: &[usize]) -> Vec<usize> {
-    let max = array.iter().max().unwrap_or(&0);
+pub fn counting_sort<T: Keyed + Ord + Copy + Default>(array: &[T]) -> Vec<T> {
+    let max: usize = array.iter().map(|item: &T| item.key()).max().unwrap_or(0);
 
-    let mut count = vec![0; max + 1];
-    let mut output = vec![0; array.len()];
+    let mut count: Vec<usize> = vec![0; max + 1];
+    let mut output: Vec<T> = vec![T::default(); array.len()];
 
     for &element in array.iter() {
-        count[element] += 1;
+        count[element.key()] += 1;
     }
 
     for i in 1..max + 1 {
@@ -13,7 +13,7 @@ pub fn counting_sort(array: &[usize]) -> Vec<usize> {
     }
 
     for i in (0..array.len()).rev() {
-        let j = array[i];
+        let j = array[i].key();
         count[j] -= 1;
         output[count[j]] = array[i];
     }
@@ -21,13 +21,49 @@ pub fn counting_sort(array: &[usize]) -> Vec<usize> {
     output
 }
 
+pub trait Keyed {
+    fn key(&self) -> usize;
+}
+
 #[cfg(test)]
 mod tests {
     use super::counting_sort;
+    use super::Keyed;
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Default)]
+    struct Custom {
+        key: usize,
+    }
+
+    impl Keyed for Custom {
+        fn key(&self) -> usize {
+            self.key
+        }
+    }
+
+    impl Keyed for usize {
+        fn key(&self) -> usize {
+            *self
+        }
+    }
 
     #[test]
     fn basic() {
         let array = [5, 4, 1, 6, 0];
+        let output = counting_sort(&array);
+        assert_sorted!(&output);
+    }
+
+    #[test]
+    fn basic_struct() {
+        let array = [
+            Custom { key: 5 },
+            Custom { key: 4 },
+            Custom { key: 1 },
+            Custom { key: 6 },
+            Custom { key: 0 },
+        ];
+
         let output = counting_sort(&array);
         assert_sorted!(&output);
     }
@@ -40,8 +76,38 @@ mod tests {
     }
 
     #[test]
+    fn repeated_elements_struct() {
+        let array = [
+            Custom { key: 5 },
+            Custom { key: 5 },
+            Custom { key: 1 },
+            Custom { key: 6 },
+            Custom { key: 1 },
+            Custom { key: 0 },
+            Custom { key: 2 },
+            Custom { key: 6 },
+        ];
+        let output = counting_sort(&array);
+        assert_sorted!(&output);
+    }
+
+    #[test]
     fn pre_sorted() {
         let array = [1, 2, 3, 4, 5, 6];
+        let output = counting_sort(&array);
+        assert_sorted!(&output);
+    }
+
+    #[test]
+    fn pre_sorted_struct() {
+        let array = [
+            Custom { key: 1 },
+            Custom { key: 2 },
+            Custom { key: 3 },
+            Custom { key: 4 },
+            Custom { key: 5 },
+            Custom { key: 6 },
+        ];
         let output = counting_sort(&array);
         assert_sorted!(&output);
     }

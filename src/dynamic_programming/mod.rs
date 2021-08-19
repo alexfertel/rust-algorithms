29ed7665e00257c6f029eb09546fb680ec1ece87
LEMON8 <201303998@post.au.dk>
2021-08-19 16:02:09 +0200

    Migrated algorithms from the old repository.

--- src/dynamic_programming/mod.rs
+++ src/dynamic_programming/mod.rs
@@ -1,6 +1,15 @@
 mod coin_problem;
+mod edit_distance;
+mod egg_dropping;
+mod knapsack;
+mod longest_common_subsequence;
 mod rod_cutting;
 
 pub use self::coin_problem::coin_problem;
+pub use self::edit_distance::edit_distance;
+pub use self::edit_distance::edit_distance_se;
+pub use self::egg_dropping::egg_drop;
+pub use self::knapsack::knapsack;
+pub use self::longest_common_subsequence::longest_common_subsequence;
 pub use self::rod_cutting::rod_cutting;
 pub use self::rod_cutting::rod_cutting_recursive;

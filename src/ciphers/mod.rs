52aa5208ae3da41b0b727622fe33e8b30accab5a
Maria <94175191+Maria-Error-404@users.noreply.github.com>
2022-01-29 02:09:44 +0100

    Add files via upload

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,7 +1,13 @@
 mod caesar;
 mod rot13;
 mod transposition;
+mod vigenere;
+mod morse_code;
+
 
 pub use self::caesar::caesar;
 pub use self::rot13::rot13;
 pub use self::transposition::transposition;
+pub use self::vigenere::vigenere;
+pub use self::morse_code::encode;
+

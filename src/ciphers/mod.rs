8b34dc83cefa7fb35aca8890359ce60502e1dcad
Maria <94175191+Maria-Error-404@users.noreply.github.com>
2022-01-30 14:45:11 +0100

    Trying now

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,11 +1,11 @@
 mod caesar;
+mod morse_code;
 mod rot13;
 mod transposition;
-mod morse_code;
 mod vigenere;
 
 pub use self::caesar::caesar;
+pub use self::morse_code::encode;
 pub use self::rot13::rot13;
 pub use self::transposition::transposition;
-pub use self::morse_code::encode;
 pub use self::vigenere::vigenere;

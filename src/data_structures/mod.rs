84f942ece36b9f7419d737382dd55312afdbcd21
zain2323 <zainsiddiqui2323@gmail.com>
2022-05-07 20:06:59 +0500

    Reformatted the code correctly.

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -2,7 +2,7 @@ mod heap;
 mod queue;
 mod stack;
 
-pub use self::queue::Queue;
-pub use self::stack::Stack;
 pub use self::heap::MaxHeap;
 pub use self::heap::MinHeap;
+pub use self::queue::Queue;
+pub use self::stack::Stack;

8d089f87269a860f3de05231dc5eda1dedd0c921
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2022-06-07 00:24:13 +0530

    fixed typo in the file

--- src/searching/linear_search.rs
+++ src/searching/linear_search.rs
@@ -16,11 +16,11 @@
 
 pub fn linear_search<T: PartialEq>(array: &[T], item: &T) -> Option<usize> {
     // iterating over the given array using `iter` method and for extracting the index using `enumerate` method
-    for(index, element) in array.iter().enumerate(){
+    for (index, element) in array.iter().enumerate(){
         // checking if the target item is the same array element
         if item == element {
             // return the index number of the the array as an option
-            Some(index);
+            return Some(index)
         }
     }
     // return None if item not found in the array
@@ -63,21 +63,21 @@ mod tests{
     /// #### Searching for strings
     fn search_strings(){
         // Defining the test string array
-        let test_array: [String; 4] =  vec!["a", "b", "c", "Hello"];
+        let test_array =  vec!["a", "b", "c", "Hello"];
 
         // Searching for "Hello" in the array
         // expected index = 3
-        let index = linear_search(&test_array, &"Hello".to_string());
+        let index = linear_search(&test_array, &"Hello");
         assert_eq!(index, Some(3));
 
         // Searching for "c" in the array
         // expected index = 2
-        let index = linear_search(&test_array, &"c".to_string());
+        let index = linear_search(&test_array, &"c");
         assert_eq!(index, Some(2));
 
         // Searching for "a" in the array
         // expected index = 0 
-        let index = linear_search(&test_array, &"a".to_string());
+        let index = linear_search(&test_array, &"a");
         assert_eq!(index, Some(0));
     }
 
@@ -85,21 +85,21 @@ mod tests{
     /// #### Searching for integers
     fn search_integers(){
         // Defining the test integer array
-        let test_array: [isize; 4] =  vec![1, 2, 5, 735, 72, 63];
+        let test_array =  vec![1, 2, 5, 735, 72, 63];
 
         // Searching for `735` in the array
         // expected index = 3
-        let index = linear_search(&test_array, &735);
+        let mut index = linear_search(&test_array, &735);
         assert_eq!(index, Some(3));
 
         // Searching for `63` in the array
         // expected index = 5
-        let index = linear_search(&test_array, &63);
+        index = linear_search(&test_array, &63);
         assert_eq!(index, Some(5));
 
         // Searching for `1` in the array
         // expected index = 0 
-        let index = linear_search(&test_array, &1);
+        index = linear_search(&test_array, &1);
         assert_eq!(index, Some(0));
     }
 }

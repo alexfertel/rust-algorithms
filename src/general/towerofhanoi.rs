a21cb9c85b3e0a1c32b9b934ea6ff8cc405dfc21
austin <austincardoza66@gmail.com>
2022-09-12 13:26:00 +0530

    added tower of hanoi

--- src/general/towerofhanoi.rs
+++ src/general/towerofhanoi.rs
@@ -7,16 +7,20 @@
 fn towerofhanoi(n: i32, from: i32, to: i32, via: i32) {
     if n > 0 {
   //shifting disks from pole 1 to 3.
-        move_(n - 1, from, via, to);
+        towerofhanoi(n - 1, from, via, to);
         println!("Move disk from pole {} to pole {}", from, to);
   //shifting disks from pole 3 to 2.
-        move_(n - 1, via, to, from);
+        towerofhanoi(n - 1, via, to, from);
     }
   }
   
-  //main function
-  fn main() {
-    towerofhanoi(4, 1,2,3);
+  #[cfg(test)]
+  mod test {
+      use super::*;
+    #[test]
+    fn towerofhanoi(4,1,2,3) 
+        
   }
+
   
   //Time complexity of the above program is approximately (2^n).

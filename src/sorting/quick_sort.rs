3ab1b48138b24b9dc86442f2bc94968e6a77374d
unknown <lrichxo@rediffmail.com>
2022-09-26 12:08:28 +0530

    Added Quicksort documentation

--- src/sorting/quick_sort.rs
+++ src/sorting/quick_sort.rs
@@ -1,3 +1,14 @@
+/// QuickSort is a Divide and Conquer algorithm. It picks an element as
+/// a pivot and partitions the given array around the picked pivot. 
+/// There are many different versions of quickSort that pick pivot in different ways. 
+/// parameters takes an array
+/// The key process in quickSort is a partition(). 
+/// The target of partitions is, given an array and an element x of an array as the pivot, 
+/// put x at its correct position in a sorted array and put all smaller elements (smaller than x) before x, 
+/// and put all greater elements (greater than x) after x. All this should be done in linear time.
+/// Quicksort's  time complexity is O(n*logn) .
+
+
 pub fn quick_sort<T: Ord>(array: &mut [T]) {
     match array.len() {
         0 | 1 => return,

74d52572730d2afba0392d81163c29746b3eb85f
Alexander Gonzalez <alexfertel97@gmail.com>
2022-11-12 18:32:09 +0200

    lint: fix formatting

--- src/graphs/mod.rs
+++ src/graphs/mod.rs
@@ -1,7 +1,7 @@
 mod breadth_first_search;
 mod depth_first_search;
-mod representation;
 mod dijkstra;
+mod representation;
 
 pub use self::breadth_first_search::breadth_first_search;
 pub use self::depth_first_search::depth_first_search;

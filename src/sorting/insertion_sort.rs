4a74260d2f441aa28f8ee8986789714f9b98c6a4
JoelStanleyD <86356662+JoelStanleyD@users.noreply.github.com>
2022-11-12 21:52:04 +0530

    feat: add docs for bubble and insertion sort (#44)

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,6 +1,16 @@
+// Insertion sort divides the array into sorted and unsorted parts.
+// Values from the unsorted parts are placed in the correct position in the sorted part
+// time complexity is O(N^2)
+// Auxiliary space is O(1)
+
 pub fn insertion_sort<T: Ord>(array: &mut [T]) {
+    // initialise the array
     for i in 0..array.len() {
         let mut j = i;
+        // Move elements of arr[0..i-1],  
+        // that are greater than key, to one 
+        // position ahead of their 
+        // current position
         while j > 0 && array[j] < array[j - 1] {
             array.swap(j, j - 1);
             j -= 1;

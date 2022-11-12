002fd15cc3311b215782b15edee4e011c9aad2b4
Alexander Gonzalez <alexfertel97@gmail.com>
2022-11-12 18:25:11 +0200

    lint(bubble_sort): fix formatting

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,10 +1,12 @@
-// It sorts the array by repeatedly comparing the adjacent elements and swapping them if they are in the wrong order
+// It sorts the array by repeatedly comparing the
+// adjacent elements and swapping them if they are
+// in the wrong order.
 // Time complexity is O(N^2)
 // Auxiliary space is O(1)
 
 pub fn bubble_sort<T: Ord>(array: &mut [T]) {
     for i in 0..array.len() {
-        // Last i elements are already in place
+        // Last i elements are already in place.
         for j in 0..array.len() - 1 - i {
             if array[j] > array[j + 1] {
                 array.swap(j, j + 1);

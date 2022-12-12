e388ca8958b14c7114fafdca48f599f16fad2351
luizasilveira <luizars7@hotmail.com>
2022-12-11 22:36:09 -0300

    Add docs to Sorting Algorithms

--- src/sorting/selection_sort.rs
+++ src/sorting/selection_sort.rs
@@ -1,12 +1,15 @@
 pub fn selection_sort<T: Ord>(array: &mut [T]) {
+    // Loop through each element in the array.
     for i in 0..array.len() {
+        // The current element is the starting minimum element.
         let mut smallest_idx = i;
+        // Loop through the remaining elements, if any of them is less than the current minimum, we update the current minimum.
         for j in i + 1..array.len() {
             if array[j] < array[smallest_idx] {
                 smallest_idx = j;
             }
         }
-
+        // We can then swap the minimum element with the current element.
         array.swap(i, smallest_idx);
     }
 }

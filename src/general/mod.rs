c9d3f8ddb29fbc90fbbb69907deb85d6d62c3469
Strix12 <t.kotila2202@gmail.com>
2023-05-14 15:41:32 -0400

    added naive pattern matching algorithm

--- src/general/mod.rs
+++ src/general/mod.rs
@@ -2,6 +2,7 @@ mod convex_hull;
 mod hanoi;
 mod huffman_encoding;
 mod kmeans;
+mod naive;
 mod nqueens;
 mod two_sum;
 
@@ -9,5 +10,6 @@ pub use self::convex_hull::convex_hull_graham;
 pub use self::hanoi::hanoi;
 pub use self::huffman_encoding::HuffmanDictionary;
 pub use self::kmeans::{f32, f64};
+pub use self::naive::naive;
 pub use self::nqueens::nqueens;
 pub use self::two_sum::two_sum;

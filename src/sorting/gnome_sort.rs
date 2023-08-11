33e35efdb9a7e7c51db7f1855dec39afc4aecea5
Abey Bernard <abeybernard321@gmai.com>
2023-08-11 20:47:19 +0530

    Brief description of changes

--- src/sorting/gnome_sort.rs
+++ src/sorting/gnome_sort.rs
@@ -1,27 +1,17 @@
-use std::cmp;
-
-pub fn gnome_sort<T>(arr: &[T]) -> Vec<T>
+pub fn gnome_sort<T>(arr: &mut [T])
 where
-    T: cmp::PartialEq + cmp::PartialOrd + Clone,
+    T: cmp::PartialOrd,
 {
-    let mut arr = arr.to_vec();
-    let mut i: usize = 1;
-    let mut j: usize = 2;
+    let mut i = 1;
 
     while i < arr.len() {
-        if arr[i - 1] < arr[i] {
-            i = j;
-            j = i + 1;
+        if i == 0 || arr[i - 1] <= arr[i] {
+            i += 1;
         } else {
             arr.swap(i - 1, i);
             i -= 1;
-            if i == 0 {
-                i = j;
-                j += 1;
-            }
         }
     }
-    arr
 }
 
 #[cfg(test)]
@@ -32,19 +22,22 @@ mod tests {
 
     #[test]
     fn odd_number_of_elements() {
-        let res = gnome_sort(&vec!["d", "a", "c", "e", "b"]);
-        assert_eq!(res, vec!["a", "b", "c", "d", "e"]);
+        let mut arr = vec!["d", "a", "c", "e", "b"];
+        gnome_sort(&mut arr);
+        assert_eq!(arr, vec!["a", "b", "c", "d", "e"]);
     }
 
     #[test]
     fn one_element() {
-        let res = gnome_sort(&vec![3]);
-        assert_eq!(res, vec![3]);
+        let mut arr = vec![3];
+        gnome_sort(&mut arr);
+        assert_eq!(arr, vec![3]);
     }
 
     #[test]
     fn empty() {
-        let res = gnome_sort(&Vec::<u8>::new());
-        assert_eq!(res, vec![]);
+        let mut arr: Vec<u8> = Vec::new();
+        gnome_sort(&mut arr);
+        assert_eq!(arr, vec![]);
     }
 }

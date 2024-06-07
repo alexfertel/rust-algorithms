bd29d6d9c1b63dc0c6f19591e56a3b8c946daeed
ScriptAndCompile <scriptandcompile@gmail.com>
2024-06-07 14:30:12 -0700

    Added documentation for another_rot13.

--- src/ciphers/another_rot13.rs
+++ src/ciphers/another_rot13.rs
@@ -1,3 +1,28 @@
+/// Applies the ROT13 cipher to the given text.
+///
+/// ROT13 is a simple letter substitution cipher that replaces a letter with the 13th letter after it in the alphabet.
+/// The cipher wraps around, so 'Z' becomes 'M' and 'z' becomes 'm'.
+/// Non-alphabetic characters are left unchanged.
+///
+/// # Arguments
+///
+/// * `text` - The text to be encrypted using ROT13 cipher.
+///
+/// # Returns
+///
+/// The encrypted text.
+///
+///  # Example
+///
+/// ```rust
+/// use rust_algorithms::ciphers::another_rot13;
+///
+/// let text = "ABCzyx";
+/// let encrypted = another_rot13(text);
+///
+/// assert_eq!(encrypted, "NOPmlk");
+/// ```
+///
 pub fn another_rot13(text: &str) -> String {
     let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";

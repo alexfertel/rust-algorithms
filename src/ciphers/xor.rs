bbebf73cbae52b0ecd643dd193ec0236849ee9ca
ScriptAndCompile <scriptandcompile@gmail.com>
2024-06-11 11:29:34 -0700

    Added documentation to xor.rs

--- src/ciphers/xor.rs
+++ src/ciphers/xor.rs
@@ -1,3 +1,23 @@
+/// XOR cipher
+///
+/// # Arguments
+///
+/// * `text` - A string slice that holds the text to be ciphered.
+/// * `key` - A u8 that holds the key to be used for ciphering.
+///
+/// # Returns
+///
+/// * A String that holds the ciphered text.
+///
+/// # Example
+/// ```rust
+/// use rust_algorithms::ciphers::xor;
+///
+/// let test_string = "The quick brown fox jumps over the lazy dog";
+/// let ciphered_text = xor(test_string, 64);
+///
+/// assert_eq!(test_string, xor(&ciphered_text, 64));
+/// ```
 pub fn xor(text: &str, key: u8) -> String {
     text.chars().map(|c| ((c as u8) ^ key) as char).collect()
 }
@@ -11,12 +31,5 @@ mod tests {
         let test_string = "test string";
         let ciphered_text = xor(test_string, 32);
         assert_eq!(test_string, xor(&ciphered_text, 32));
-    }
-
-    #[test]
-    fn test_every_alphabet_with_space() {
-        let test_string = "The quick brown fox jumps over the lazy dog";
-        let ciphered_text = xor(test_string, 64);
-        assert_eq!(test_string, xor(&ciphered_text, 64));
     }
 }
